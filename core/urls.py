b28c5068b82e1c33de35d6a56f0c81162237fd7b
Mohamad Ep <mamad_ep@yahoo.com>
2025-09-15 16:51:35 +0330

    Chore: Add swagger urls & Configuration docs

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,26 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+# ________________________________________________
 
+# swagger config
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+# ________________________________________________
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -28,6 +47,11 @@ urlpatterns = [
     
     # Basic Auth for DRF
     path("api-auth/", include("rest_framework.urls")),
+    
+	# swagger
+   	path('swagger/outout.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   	path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   	path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
